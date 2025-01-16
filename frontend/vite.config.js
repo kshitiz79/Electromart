b91c2f9460e57c6464ea6e386b837d350b824227
Kshitiz Kumar Maurya <117600399+kshitiz79@users.noreply.github.com>
2025-01-16 20:57:02 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend', // Adjust if your app is hosted on a subdirectory
-  build: {
-    outDir: 'dist', // Ensure the output directory is correct
-  },
+  root: './',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 });
